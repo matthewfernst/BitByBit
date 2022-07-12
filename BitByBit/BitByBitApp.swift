e70a3b3f40d9a5e33ea98ecd6177e3966cf4da71
matthewfernst <matthew.f.ernst@gmail.com>
2022-07-11 21:54:09 -0700

    Initial Commit

---
+++ BitByBit/BitByBitApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitByBitApp.swift
+//  BitByBit
+//
+//  Created by Matthew Ernst on 7/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BitByBitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitByBit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitByBit
+//
+//  Created by Matthew Ernst on 7/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
